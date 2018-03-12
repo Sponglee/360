79b7b857d91feb4fb5229bf2aee2c50b3b053f73
Sponglee <acearutyunyan@gmail.com>
2018-03-13 01:42:43 +0300

    13.03.18
    Added gravity
    
    Input
    
    Respawn and merging

---
+++ Assets/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : Singleton<GameManager> {
+
+    [SerializeField]
+    private GameObject wheel;
+    public GameObject squarePrefab;
+    public Transform wheelSquares;
+
+    
+    public Text nextScore;
+    public static int next_score;
+    // Use this for initialization
+    void Start () {
+        next_score = Random.Range(1, 20);
+        nextScore.text = next_score.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            wheel.transform.Rotate(Vector3.forward, 20f);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            wheel.transform.Rotate(Vector3.forward, -20f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(squarePrefab, new Vector3(0f,0f,0f) , Quaternion.identity);
+        }
+    }
+
+
+    public void Merge(GameObject first)
+    {
+        first.transform.SetParent(GameManager.Instance.wheelSquares.transform);
+        first.GetComponent<Square>().Score *= 2;
+
+        first.GetComponent<SpriteRenderer>().color = new Color32(200, 200, 200, 255);
+        first.isStatic = true;
+        
+        Debug.Log("WIN");
+
+    }
+}
---
+++ Assets/_Scripts/Square.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Square : MonoBehaviour {
+
+    [SerializeField]
+    private bool bottom=false;
+    private int row;
+    public int Row
+    { get { return row; } set { row = value; } }
+    [SerializeField]
+    private int score;
+    public int Score
+    { get {return score;} set {score = value;} }
+
+   
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+
+        GameManager.Instance.nextScore.text = GameManager.next_score.ToString();
+        if (!bottom)
+        {
+         
+            GameManager.next_score = Random.Range(1, 20);
+            score = GameManager.next_score;
+           
+
+        }
+
+
+       
+        gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = score.ToString();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+     
+      
+	}
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("wheel") || other.gameObject.GetComponent<Square>().row == 0) 
+        {
+            if (transform != null)
+                transform.SetParent(GameManager.Instance.wheelSquares.transform);
+            gameObject.GetComponent<SpriteRenderer>().color = new Color32(200, 200, 200, 255);
+            gameObject.isStatic = true;
+            this.row = 0;
+        }
+
+        if (other.gameObject.CompareTag("square"))
+        {
+           
+                if (this.score == other.gameObject.GetComponent<Square>().Score)
+                {
+                Debug.Log("BOOP");
+                GameManager.Instance.Merge(gameObject);
+                gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = score.ToString();
+                Destroy(other.gameObject);
+                }
+           
+        }
+
+       
+        
+    }
+}
