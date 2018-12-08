d5aae04a48f51a35dfd6b48f7c7d0b4dbdaeb48d
SolidHinken <solidhinken@gmail.com>
2018-12-08 23:39:37 +0300

    08.12.18
    Build 1.1 (1)
    
    Ads, CrossPlatform Screenshot sharing

--- Assets/_Scripts/CoinManager.cs
+++ Assets/_Scripts/CoinManager.cs
@@ -130,7 +130,7 @@ public class CoinManager : Singleton<CoinManager> {
                 }
             case ShowResult.Skipped:
                 {
-                    //CoinManager.Instance.Coins += partAdWatch;
+                    CoinManager.Instance.Coins += partAdWatch;
                     Time.timeScale = 1;
 
                     break;
--- Assets/_Scripts/NativeShareScript.cs
+++ Assets/_Scripts/NativeShareScript.cs
@@ -1,107 +1,99 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.IO;
-using UnityEngine.UI;
 using System.Runtime.InteropServices;
+
+
 
 public class NativeShareScript : MonoBehaviour
 {
-    public GameObject CanvasShareObj;
-
-    //For sharing
-    public GameObject snap;
-    public GameObject pause;
-    public GameObject powerUpPanel;
-
-
-    private bool isProcessing = false;
-    private bool isFocus = false;
-
-
     public string subject, ShareMessage, url;
+    private bool isProcessing = false;
     public string ScreenshotName = "screenshot.png";
-   
 
-
-
-public void ShareBtnPress()
+    public void ShareScreenshotWithText()
     {
-        if (!isProcessing && !GameManager.Instance.MenuUp)
-        {
-            CanvasShareObj.SetActive(true);
-            StartCoroutine(CallSocialShareRoutine());
+        // Share();
+    }
+
+    public void Share()
+    {
+    #if UNITY_ANDROID
+    if(!isProcessing)
+    StartCoroutine( ShareScreenshot() );
+    #elif UNITY_IOS
+            if (!isProcessing)
+                StartCoroutine(CallSocialShareRoutine());
+    #else
+    Debug.Log("No sharing set up for this platform.");
+    #endif
         }
-    }
-
-
-    public struct ConfigStruct
+    #if UNITY_ANDROID
+    public IEnumerator ShareScreenshot()
     {
-        public string title;
-        public string message;
-    }
-    [DllImport("__Internal")] private static extern void showAlertMessage(ref ConfigStruct conf);
-    public struct SocialSharingStruct
+    isProcessing = true;
+    // wait for graphics to render
+    yield return new WaitForEndOfFrame();
+    string screenShotPath = Application.persistentDataPath + "/" + ScreenshotName;
+    Application.CaptureScreenshot(ScreenshotName);
+    yield return new WaitForSeconds(1f);
+    if(!Application.isEditor)
     {
-        public string text;
-        public string url;
-        public string image;
-        public string subject;
+    AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+    AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+    intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+    AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+    AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + screenShotPath);
+    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+    intentObject.Call<AndroidJavaObject>("setType", "image/png");
+    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), ShareMessage);
+    AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+    AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "YO! I Love Who Lurks!");
+    currentActivity.Call("startActivity", jChooser);
     }
-    [DllImport("__Internal")] private static extern void showSocialSharing(ref SocialSharingStruct conf);
-    public void CallSocialShare(string title, string message)
-    {
-        ConfigStruct conf = new ConfigStruct();
-        conf.title = title;
-        conf.message = message;
-        showAlertMessage(ref conf);
-        isProcessing = false;
+    isProcessing = false;
     }
-    public static void CallSocialShareAdvanced(string defaultTxt, string subject, string url, string img)
-    {
-        SocialSharingStruct conf = new SocialSharingStruct();
-        conf.text = defaultTxt;
-        conf.url = url;
-        conf.image = img;
-        conf.subject = subject;
-        showSocialSharing(ref conf);
-    }
-    IEnumerator CallSocialShareRoutine()
-    {
-        isProcessing = true;
-        string screenShotPath = Application.persistentDataPath + "/" + ScreenshotName;
-        ScreenCapture.CaptureScreenshot(ScreenshotName);
-        yield return new WaitForSeconds(1f);
-        CallSocialShareAdvanced(ShareMessage, subject, url, screenShotPath);
-
-        //yield return new WaitUntil(() => isFocus);
-        CanvasShareObj.SetActive(false);
-
-        GameManager.Instance.shareButton.SetActive(true);
-        GameManager.Instance.powerUpPanel.SetActive(true);
-        GameManager.Instance.ui.transform.GetChild(0).gameObject.SetActive(true);
-
-
-
-        isProcessing = false;
-
-    }
-
-
-
-
-    private void OnApplicationFocus(bool focus)
-    {
-        Time.timeScale = 1;
-        isFocus = focus;
-        GameManager.Instance.shareButton.SetActive(true);
-        GameManager.Instance.ui.transform.GetChild(0).gameObject.SetActive(true);
-        GameManager.Instance.powerUpPanel.SetActive(true);
-    }
+    #endif
+    #if UNITY_IOS
+        public struct ConfigStruct
+        {
+            public string title;
+            public string message;
+        }
+        [DllImport("__Internal")] private static extern void showAlertMessage(ref ConfigStruct conf);
+        public struct SocialSharingStruct
+        {
+            public string text;
+            public string url;
+            public string image;
+            public string subject;
+        }
+        [DllImport("__Internal")] private static extern void showSocialSharing(ref SocialSharingStruct conf);
+        public void CallSocialShare(string title, string message)
+        {
+            ConfigStruct conf = new ConfigStruct();
+            conf.title = title;
+            conf.message = message;
+            showAlertMessage(ref conf);
+            isProcessing = false;
+        }
+        public static void CallSocialShareAdvanced(string defaultTxt, string subject, string url, string img)
+        {
+            SocialSharingStruct conf = new SocialSharingStruct();
+            conf.text = defaultTxt;
+            conf.url = url;
+            conf.image = img;
+            conf.subject = subject;
+            showSocialSharing(ref conf);
+        }
+        IEnumerator CallSocialShareRoutine()
+        {
+            isProcessing = true;
+            string screenShotPath = Application.persistentDataPath + "/" + ScreenshotName;
+            ScreenCapture.CaptureScreenshot(ScreenshotName);
+            yield return new WaitForSeconds(1f);
+            CallSocialShareAdvanced(ShareMessage, subject, url, screenShotPath);
+        }
+    #endif
 }
-
-
-
-
-
-
-
